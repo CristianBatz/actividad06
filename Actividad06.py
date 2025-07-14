fbf9b546403c073ec99ad64d46bb6b944b3bb832
CristianBatz <ce.sbatz@udeo.edu.gt>
2025-07-14 16:52:03 -0600

    terminando de corregir errores en el codigo ademas de agregar el prevenir errores en la parte de ingreso sobre el codigo,precio y stock ademas de agregar la parte de ingresar cuantos productos quiere registrar y el completar la parte de la cantidad de productos que hay de cada categoria

--- Actividad06.py
+++ Actividad06.py
@@ -12,19 +12,31 @@ while opcion != 5:
 
     if opcion == 1:
         print("=== Productos ===")
-        codigo = input("Ingrese el codigo del producto: ")
-        nombre = input("Ingrese el nombre del producto: ")
-        categoria = input("Ingrese la categoria del producto: ")
-        talla = input("Ingrese la talla del producto: ")
-        stock = int(input("Ingrese el stock del producto: "))
-        precio = float(input("Ingrese el precio del producto: "))
-        productos[codigo] = {
-            "nombre": nombre,
-            "categoria": categoria,
-            "talla": talla,
-            "stock": stock,
-            "precio": precio
-        }
+        cantidad = int(input("¿Cuantos productos desea ingresar?: "))
+        for i in range(cantidad):
+            print(f"producto #{i+1}")
+            codigo = input("Ingrese el codigo del producto: ")
+            while codigo in productos:
+                print("ya hay un producto con ese codigo, vuelva a intentarlo")
+                codigo = input("Ingrese el codigo del producto: ")
+            nombre = input("Ingrese el nombre del producto: ")
+            categoria = input("Ingrese la categoria del producto: ")
+            talla = input("Ingrese la talla del producto: ")
+            precio = float(input("Ingrese el precio del producto: "))
+            while precio <=0:
+                print("El precio debe ser mayor a 0")
+                precio = float(input("Ingrese el precio del producto: "))
+            stock = int(input("Ingrese el stock del producto: "))
+            while stock <=0:
+                print("El stock debe ser mayor a 0")
+                stock = int(input("Ingrese el stock del producto: "))
+            productos[codigo] = {
+                "nombre": nombre,
+                "categoria": categoria,
+                "talla": talla,
+                "precio": precio,
+                "stock": stock,
+            }
 
     elif opcion == 2:
         print("=== Buscar productos ===")
@@ -35,6 +47,7 @@ while opcion != 5:
             print(f"talla: {productos[buscando]['talla']}")
             print(f"stock: {productos[buscando]['stock']}")
             print(f"precio: {productos[buscando]['precio']}")
+            print("-----------------------------")
         else:
             print("No se encontro el producto")
 
@@ -43,9 +56,20 @@ while opcion != 5:
         print("=== Valor total del inventario ===")
         for datos in productos.values():
             total += datos['stock'] * datos['precio']
-            print(f"Valor total del inventario es de: {total}")
+        print(f"Valor total del inventario es de: {total}")
 
     elif opcion == 4:
         print("=== Cantidad de productos ===")
         print(f"Cantidad total de productos: {len(productos)}")
-
+        print("=== cantidad por categoria ===")
+        categorias = {}
+        for datos in productos.values():
+            categoriaA = datos['categoria']
+            if categoriaA not in categorias:
+                categorias[categoriaA] = 0
+            categorias[categoriaA] += 1
+        for categoriaA, cantidad in categorias.items():
+            print(f"{categoriaA}: {cantidad}")
+    elif opcion == 5:
+        print("=== Salir ===")
+        print("saliendo del programa")
